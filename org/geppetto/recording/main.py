c60ff7f163378be7dd864e94ab5cc51ad8fe45da
jrieke <johannes.rieke@gmail.com>
2014-07-24 02:12:20 +0200

    Integrated Python's unittest module

--- org/geppetto/recording/main.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'matteocantarelli'
-
-from org.geppetto.recording.test import *
-
-testCreator = CreateTestGeppettoRecording()
-testCreator.example1()
-testCreator.example2()
--- org/geppetto/recording/test/test_creator.py
+++ org/geppetto/recording/test/test_creator.py
@@ -1,18 +1,12 @@
 __author__ = 'matteocantarelli'
 
 from org.geppetto.recording import GeppettoRecordingCreator, MetaType
+import unittest
 
+class CreateTestGeppettoRecording(unittest.TestCase):
+    """This class demonstrates how to create a recording for Geppetto."""
 
-class CreateTestGeppettoRecording:
-    """
-    This class demonstrates how to create a recording for Geppetto.
-
-    """
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def example1():
+    def test_fixed_timestep(self):
         creator = GeppettoRecordingCreator('example1.h5')
         creator.add_value('a.w', 1, 'float_', 'mV', MetaType.STATE_VARIABLE)
         creator.add_value('a.b.c.param', 1, 'float_', 'mV', MetaType.PARAMETER)
@@ -24,8 +18,7 @@ class CreateTestGeppettoRecording:
         creator.add_metadata('boolean_metadata', True)
         creator.create()
 
-    @staticmethod
-    def example2():
+    def test_variable_timestep(self):
         creator = GeppettoRecordingCreator('example2.h5')
         creator.add_value('a.w', 1, 'float_', 'mV', MetaType.STATE_VARIABLE)
         creator.add_value('a.b.c.param', 1, 'float_', 'mV', MetaType.PARAMETER)
@@ -34,3 +27,7 @@ class CreateTestGeppettoRecording:
         creator.add_variable_time_step_vector([0.1, 0.2, 0.5, 0.51, 0.52, 0.6, 0.7], 'ms')
         creator.add_metadata('string_metadata', 'description or link')
         creator.create()
+
+
+if __name__ == '__main__':
+    unittest.main()  # automatically executes all methods above that start with 'test'
